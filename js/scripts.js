1ef2c086e394818d5ad737178ab0a58de3bbed97
Dustin Starke and Michael Donovan <dustin@>
2018-08-07 16:59:54 -0700

    Finish card generator functions

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,13 @@ var definitions =
   JavaScript : "a vegetable",
   Operator : "also a vegetable",
   Variable : "not a dog",
-  
+
 }
 
 $(document).ready(function(){
+  createLayout("#card-area",layout)
+  addCards()
+
   $('.flashcard').click(function(){
     if ($(this).hasClass("word-side")) {
       $(this).removeClass("word-side")
@@ -26,29 +29,32 @@ $(document).ready(function(){
     }
   })
 })
-// function createLayout(container,layoutArray) {
-//   var rows = layoutArray.length
-//   for (var r=0;r<rows;r++) {
-//     var columns = layoutArray[r].length
-//     // make the row divs
-//     $(container).append(`<div id="row-`+r+`" class="row" style="text-align:left"></div>`)
-//     // grab the row div you just made
-//     var newRow = $('#row-'+r)
-//     // put the columns in
-//     for (var c=0;c<columns;c++) {
-//       var columnWidth = layoutArray[r][c]
-//       console.log("col wid is " + columnWidth)
-//       newRow.append(`<div id="column-`+r+`-`+c+`" class="bordered col-md-`+columnWidth+`" style="text-align:right"></div>`)
-//     }
-//   }
-// }
-// function insertCards() {
-//   var panelArray = document.getElementsByClassName("flashcard word-side")
-//   for (var e=0;e<panelArray.length;e++) {
-//     var panel = panelArray[e]
-//     var wordList = Object.keys(definitions)
-//     var word = wordList[e]
-//     $('#'+panel.id).html(word)
-//   }
-//   console.log(panelArray)
-// }
+function createLayout(container,layoutArray) {
+  var rows = layoutArray.length
+  for (var r=0;r<rows;r++) {
+    var columns = layoutArray[r].length
+    // make the row divs
+    $(container).append(`<div id="row-`+r+`" class="row" style="text-align:left"></div>`)
+    // grab the row div you just made
+    var newRow = $('#row-'+r)
+    // put the columns in
+    for (var c=0;c<columns;c++) {
+      var columnWidth = layoutArray[r][c]
+      newRow.append(`<div id="column-`+r+`-`+c+`" class="bordered col-md-`+columnWidth+`" style="text-align:right"></div>`)
+    }
+  }
+}
+function addCards() {
+  var cardsNeeded = 18
+  var rows = layout.length
+  for (var r=0;r<rows;r++) {
+    var columns = layout[r].length
+    for (var c=0;c<columns;c++) {
+      var sectionID = "#column-"+r+"-"+c
+      var cardHTML = `<div class="panel panel-default flashcard word-side"><div class="panel-body"><div id="changeable-text-`+r+"-"+c+`"></div></div></div>`
+      $(sectionID).html(cardHTML)
+      $("#changeable-text-"+r+"-"+c).html("WORD " + r + " " + c)
+
+    }
+  }
+}
